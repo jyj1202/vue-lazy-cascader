1e1784342eb7004327a62b7d4ac5d8f6427a3e47
jyj <jyj1202@163.com>
2026-01-18 14:50:08 +0800

    chore: remove core-js when build lib

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    [
+      '@vue/cli-plugin-babel/preset',
+      {
+        useBuiltIns: process.env.VUE_APP_BUILD_TARGET === 'lib' ? false : 'entry',
+        corejs: 3
+      }
+    ]
   ]
 }
