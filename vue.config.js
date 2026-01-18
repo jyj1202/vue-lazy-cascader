0c09b45f4d2389f689283f81d11ebe1ccace50dc
jyj <jyj1202@163.com>
2026-01-18 14:51:33 +0800

    refactor: libraryExport use 'default'

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,26 @@
 const { defineConfig } = require('@vue/cli-service')
+
+const isLib = process.env.VUE_APP_BUILD_TARGET === 'lib'
+
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: 'warning',
-  publicPath: '/vue-lazy-cascader/'
+  publicPath: '/vue-lazy-cascader/',
+  configureWebpack: config => {
+    if (isLib) {
+      // 只将顶层 element-ui 和 vue 设为 external，其内部模块打包进库
+      config.externals = {
+        'element-ui': {
+          root: 'ELEMENT',
+          commonjs: 'element-ui',
+          commonjs2: 'element-ui',
+          amd: 'element-ui'
+        }
+      }
+      config.output = {
+        ...config.output,
+        libraryExport: 'default'
+      }
+    }
+  }
 })
